c434388e2d40f13a4f95c5a38924d9a340e34a97
Nils Wiersma <nils.wiersma@gmail.com>
2020-05-13 16:44:07 +0200

    classed example

--- bytecracker.py
+++ bytecracker.py
@@ -1,4 +1,4 @@
-from submod.aes.aes import AES, matrix2bytes
+from submod.aes.aes import AES, matrix2bytes, reverse_expand_key
 from submod.JeanGrey.phoenixAES import phoenixAES
 import random, os
 import faultmodels
@@ -27,31 +27,42 @@ intermediates = [
     # 'sb5',
     # 'sr5',
     # 'mc5',
-    # 'ark5',
+    'ark5',
     # 'sb6',
     # 'sr6',
-
     # 'mc6',
-    # 'ark6',
+    'ark6',
     # 'sb7',
     # 'sr7',
 
     # 'mc7',
-    # 'ark7',
+    'ark7',
     # 'sb8',
     # 'sr8',
 
     # 'mc8',
-    # 'ark8',
-    'sb9',
+    'ark8',
+    # 'sb9',
     # 'sr9',
     
     # 'mc9',
-    # 'ark9',
+    'ark9',
     # 'sb10',
     # 'sr10',
-    # 'ark10',
-    ]
+    'ark10',
+]
+
+models = [
+    # faultmodels.single_bit_flip,
+    # faultmodels.single_byte_corruption,
+    # faultmodels.random_multi_bit_flip,
+    # faultmodels.single_byte_multi_bit_flip,
+    faultmodels.single_col_corruption,
+    faultmodels.single_row_corruption,
+    faultmodels.double_byte_multi_bit_flip,
+    faultmodels.triple_byte_multi_bit_flip,
+    faultmodels.single_bit_every_byte,
+]
 
 key        = b'SiDeChaNneLMarVl'
 message    = b'sUpErSEcREtmESsG'
@@ -69,22 +80,22 @@ for ik in iks:
 
 
 faulted = []
-for _ in range(10000):
-    faulted.append(os.urandom(16))
-for _ in range(2):
-    faulted.append(ctx.encrypt_block(bytes(message), glitch_at='sb9', glitch=faultmodels.single_bit_flip))
-for _ in range(3):
-    faulted.append(ctx.encrypt_block(bytes(message), glitch_at='sb8', glitch=faultmodels.single_bit_flip))
-    # faulted.append(ctx.encrypt_block(bytes(message), glitch_at=intermediate, glitch=single_byte_corruption))
-    # faulted.append(ctx.encrypt_block(bytes(message), glitch_at=intermediate, glitch=single_col_corruption))
-    # faulted.append(ctx.encrypt_block(bytes(message), glitch_at=intermediate, glitch=single_byte_multi_bit_flip))
-    # faulted.append(ctx.encrypt_block(bytes(message), glitch_at=intermediate, glitch=double_byte_multi_bit_flip))
-    # faulted.append(ctx.encrypt_block(bytes(message), glitch_at=intermediate, glitch=triple_byte_multi_bit_flip))
-    # print(f'faulted    : {faulted[-1].hex()}')
+for _ in range(1000):
+    t = random.choice([1,2,3])
+    if t == 1:
+        faulted.append(ciphertext)
+    if t == 2:
+        faulted.append(os.urandom(16))
+    if t == 3:
+        faulted.append(ctx.encrypt_block(bytes(message), 
+            glitch_at=random.choice(intermediates), 
+            glitch=random.choice(models)))
+
 random.shuffle(faulted)
 
 cracker = phoenixAES.ByteCracker(ciphertext, encrypt=True, verbose=0)
 
+print('--r9--')
 # try treating as r9 faults
 for faulty in faulted:
     new_sol = cracker.crack_bytes(faulty)
@@ -92,9 +103,17 @@ for faulty in faulted:
         exit = False
         print(f'result after {cracker.counter} good faulted analyzed')
         for key in cracker.key_solutions():
-            print(phoenixAES.hexdot(key), phoenixAES.hexdot(key) == iks[-1].hex())
-            if phoenixAES.hexdot(key) == iks[-1].hex():
-                exit = True
+            print(phoenixAES.hexdot(key))
+            if None not in key:
+                # check if it's the right key
+                key_matrices = reverse_expand_key(bytes(key))
+                plain_check = AES(key_matrices[0]).decrypt_block(ciphertext)
+                if plain_check == message:
+                    print(bytes(key).hex(), '-->', key_matrices[0].hex(), ' < correct key!')
+                    exit = True
+            # print(phoenixAES.hexdot(key), phoenixAES.hexdot(key) == iks[-1].hex())
+            # if phoenixAES.hexdot(key) == iks[-1].hex():
+            #     exit = True
         if exit:
             break
 
@@ -104,19 +123,29 @@ for faulty in faulted:
 # r9 faults dont produce false positives
 for idx in range(len(cracker.solutions)):
     cracker.recovered[idx] = [] != cracker.solutions[idx]
-print(cracker.recovered)
+# print(cracker.recovered)
 
+print('--r8--')
 # check for r8 faults
 faulted2 = phoenixAES.convert_r8faults_bytes(faulted, ciphertext)
-for faulty in tqdm.tqdm(faulted2):
+# for faulty in tqdm.tqdm(faulted2):
+for faulty in faulted2:
     new_sol = cracker.crack_bytes(faulty)
     if new_sol:# and [] not in cracker.solutions:
         exit = False
         print(f'result after {cracker.counter} good faulted analyzed')
         for key in cracker.key_solutions():
-            print(phoenixAES.hexdot(key), phoenixAES.hexdot(key) == iks[-1].hex())
-            if phoenixAES.hexdot(key) == iks[-1].hex():
-                exit = True
+            print(phoenixAES.hexdot(key))
+            if None not in key:
+                # check if it's the right key
+                key_matrices = reverse_expand_key(bytes(key))
+                plain_check = AES(key_matrices[0]).decrypt_block(ciphertext)
+                if plain_check == message:
+                    print(bytes(key).hex(), '-->', key_matrices[0].hex(), ' < correct key!')
+                    exit = True
+            # print(phoenixAES.hexdot(key), phoenixAES.hexdot(key) == iks[-1].hex())
+            # if phoenixAES.hexdot(key) == iks[-1].hex():
+            #     exit = True
         if exit:
             break
 
